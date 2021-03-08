ce4b93177cb2ac5bee3a55608e70f5782c364493
sridhar <patienceprogrammer@gmail.com>
2021-03-08 17:22:17 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-from sqlalchemy.ext.declarative import declarative_base
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
